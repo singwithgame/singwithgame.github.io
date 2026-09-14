04faa86288133b06a706d40e0ae278774cf99b64
saintation <saintation@hotmail.com>
2026-09-14 11:17:18 +0900

    fix: 저장소 이름 변경에 따른 설정 수정 및 Node.js 버전 20으로 상향

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://singwithgame.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/portal/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'singwithgame', // Usually your GitHub org/user name.
-  projectName: 'portal', // Usually your repo name.
+  projectName: 'singwithgame.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
