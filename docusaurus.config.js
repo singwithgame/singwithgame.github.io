3fc055512931927b8613b5bb2183491f74acfbf0
saintation <saintation@hotmail.com>
2026-09-15 13:55:22 +0900

    chore: 카카오톡 등 메신저 공유 시 표시되는 OG 이미지를 히어로 이미지로 변경 및 불필요한 Docusaurus 기본 에셋 삭제

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,7 +114,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/hero.jpg',
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: false,
